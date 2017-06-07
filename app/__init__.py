11bb757364b1bf325a317d0a40e138d4ac7288b5
Abel-Huang <497720975@qq.com>
2017-06-07 21:59:08 +0800

    start build user authentication

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,16 @@ from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import config
+from flask_login import LoginManager
 
 bootstrap=Bootstrap()
 mail=Mail()
 moment=Moment()
 db=SQLAlchemy()
+
+login_manager=LoginManager()
+login_manager.session_protection='strong'
+login_manager.login_view='auth.login'
 
 def create_app(config_name):
     app=Flask(__name__)
@@ -21,9 +26,14 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
+
     # 注册蓝本
-    from . import main as main_blueprint
+    from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    #  附加auth蓝本
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 #     附加路由和自定义的错误页面
 
     return app
--- app/models.py
+++ app/models.py
@@ -1,21 +1,51 @@
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_sqlalchemy import SQLAlchemy
-import flask
-app=flask.Flask(__name__)
-db = SQLAlchemy(app)
+from . import db
+from . import login_manager
 # db model
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 class Role(db.Model):
     __table_name__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+
     def __repr__(self):
         return '<Role %r>' % self.name
 
 
 class User(db.Model):
     __table_name__ = 'users'
+    # 定义字段
     id = db.Column(db.Integer, primary_key=True)
+    email=db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    password_hash=db.Column(db.String(128))
+    role_id=db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    @property
+    def password(self):
+        raise  AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash=generate_password_hash(password)
+
+    # 返回true 则密码为正确
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+
+
+
+
+
+
+
 
